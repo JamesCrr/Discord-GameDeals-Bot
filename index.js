e0a06bf73009a66e5a142be2180c35c197545c33
Wx <g0546699m@gmail.com>
2020-08-25 12:18:16 +0800

    Made sure bot only posts newest deals. also made it post on it's own channel

--- index.js
+++ index.js
@@ -1,34 +1,74 @@
 const getResults = require("./scrapper");
 const config = require("./config.json");
 const discord = require("discord.js");
+const discordChannelTargetName = "game-deals";
 const discordClient = new discord.Client();
 discordClient.login(config.botKey);
 
-const timerFunc = async () => {
-    const results = await getResults();
-    const channelTarget = discordClient.channels.cache.find(channel => channel.name === "game-deals");
-    var msg = ""
-    //console.log(results);
-    for(var i = 0; i < results.titles.length; ++i) {
-        msg = "**" + results.titles[i] + "**" + "\n";
-        msg += results.dealLinks[i];
-        channelTarget.send(msg);
-    }
-}
+var previousResults = {
+    titles: null,
+    dealLinks: null,
+    redditLinks: null,
+};
 var myTimer;
+var channelTarget;
+const timerFunc = async () => {
+    // Channel got deleted
+    if (getChannel() === false) 
+        setUpChannel();
+
+    const results = await getResults();
+    if (previousResults.titles === null)
+        recordLatestTitle(results);
+    else if (previousResults.titles !== results.titles[0]) {
+        var msg = ""
+        msg = "**" + results.titles[0] + "**" + "\n";
+        msg += results.dealLinks[0];
+        channelTarget.send(msg);
+        recordLatestTitle(results);
+    }
+    console.log("No New Deals..");
+}
+const recordLatestTitle = (newResults) => {
+    previousResults.titles = newResults.titles[0];
+    previousResults.dealLinks = newResults.dealLinks[0];
+    previousResults.redditLinks = newResults.redditLinks[0];
+}
+const setUpChannel = () => {
+    if (getChannel() === true)
+        return;
+    // Create a new text channel
+    const discordGuild = discordClient.guilds.cache.first();
+    discordGuild.channels.create(discordChannelTargetName, { 
+        type: "text",
+        topic: "Game Deals Channel" 
+    })
+    .then(() => {
+        channelTarget = discordClient.channels.cache.find(channel => channel.name === discordChannelTargetName);
+        channelTarget.send("I am using this Channel to send you Deals!\nPlease don't delete this Channel.");
+        console.log("Channel Target: " + channelTarget);
+    })
+    .catch(console.error);
+}
+const getChannel = () => {
+    const channelExists = discordClient.channels.cache.find(channel => channel.name === discordChannelTargetName);
+    if (channelExists === undefined)
+        return false;
+    return true;
+}
 
 discordClient.once("ready", () => {
     console.log("ONLINE")
+    setUpChannel();
+    setInterval(timerFunc, 3600000);    // 3 600 000 miliseconds = 1 hr
 });
 discordClient.on("message", message => {
     if(message.author.bot)
         return;
 
-    if(message.content === "!starttimer"){
-        setInterval(timerFunc, 1000);
-    }   
-    else if(message.content === "!stoptimer"){
+    if(message.content === "!exit"){
         clearInterval(myTimer);
-        message.channel.send("Timer Cleared!");
-    }
+        myTimer = null;
+        discordClient.destroy();
+    }   
 });
